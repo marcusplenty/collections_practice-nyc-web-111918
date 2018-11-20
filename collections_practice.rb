129299dd83c3f76d42e0a4335cd0189b2f597cef
Marcus Plenty <marcusplenty@gmail.com>
2018-11-20 00:49:11 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -55,11 +55,12 @@ end
 def add_s(array)
   y = []
   array.each_with_index do |string,i|
-    if i != 2 
-      x = string + "s"
-    else
+    if i == 2 
       x = string
+    else
+      x = string + "s"
     end
     y << x
   end
+  return y
 end
